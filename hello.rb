5c308c48d4449688a3034536789331964f7d4249
Kevin Bongart <contact@kevinbongart.net>
2010-10-27 10:01:18 +0200

    Fixed a lot of things.

--- hello.rb
+++ hello.rb
@@ -20,21 +20,27 @@ def get_band_name
   puts pages.to_s
   res = get_http('http://www.wikirandom.org/json&pages=' + pages.to_s)
   json = JSON.parse res.body
-  band_name = json['data'][rand(pages)]['title']
-
+  random = rand(pages)
+  url = json['data'][random]['url']
+  band_name = json['data'][random]['title']
   band_name.gsub(/ \(.*\)$/, '')
+
+  {"band_name" => band_name, "url" => url}
 end
 
 def get_album_name
   res = get_http('http://www.quotationspage.com/random.php3')
   body = Hpricot res.body
-  quote = body.search("dt[@class*=quote]").last.search("a").first.inner_html
+  a = body.search("dt[@class*=quote]").last.search("a").first
+  url = 'http://www.quotationspage.com/' + a.attributes["href"]
+  quote = a.inner_html
   last_words = quote.split(/ /)
   last_words = last_words.last(4)
   last_words.first.capitalize!
   last_words.last.gsub!(/\./, '')
+  album_name = last_words.join(" ")
 
-  last_words.join(" ")
+  {"album_name" => album_name, "url" => url}
 end
 
 def get_album_cover
@@ -42,19 +48,24 @@ def get_album_cover
   body = Hpricot res.body
   a = body.search("span[@class*=photo_container pc_m]")[2].at("a")
   album_cover = a.at("img")
-
-  url = a.attributes["href"]
-  album_cover = album_cover.attributes["src"]
+  url = 'http://www.flickr.com' + a.attributes["href"]
+  album_cover = album_cover.attributes["src"].gsub(/\_m/, "_z")
 
   {"album_cover" => album_cover, "url" => url}
 end
 
 get '/' do
-  @band_name    = get_band_name
-  @album_name   = get_album_name
-  cover         = get_album_cover
-  @album_cover  = cover["album_cover"]
-  @url          = cover["url"]
+  @band_name        = get_band_name
+  @band_name_url    = @band_name["url"]
+  @band_name        = @band_name["band_name"]
+
+  @album_name       = get_album_name
+  @album_name_url   = @album_name["url"]
+  @album_name       = @album_name["album_name"]
+
+  @cover            = get_album_cover
+  @album_cover      = @cover["album_cover"]
+  @album_cover_url  = @cover["url"]
 
   erb :hello
 end
