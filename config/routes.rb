d70be379553511f1ae1dc7f20729ff2b160e8999
Kevin Bongart <contact@kevinbongart.net>
2014-02-25 08:48:14 -0500

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Ourbandiscalled::Application.routes.draw do
-  resources "record", only: [:new, :show]
   get "/:id", to: "record#short_url", constraints: { id: /\d+/ }, as: :short_url
   get "/:slug", to: "record#show", as: :slug
   root "record#new"
