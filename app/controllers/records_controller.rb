8173d13c8ed7999899662efb4125b592b5780f35
Kevin Bongart <contact@kevinbongart.net>
2019-01-22 08:56:31 -0500

    Specs

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,10 @@
 class RecordsController < ApplicationController
-  def index
-    @records = Record.order(views: :desc)
-    @record = @records.first
-  end
-
   def new
-    redirect_to slug_path Record.create
+    redirect_to(Record.create)
   end
 
   def show
-    @record = Record.find_by_slug params[:slug]
+    @record = Record.find_by_slug!(params[:id])
     @record.increment!(:views)
-  end
-
-  def short_url
-    redirect_to slug_path Record.find params[:id]
   end
 end
